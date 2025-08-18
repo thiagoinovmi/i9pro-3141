a078278eec798f24d175adf7adf4d25f51cd3e47
thiagoinovmi <61652481+thiagoinovmi@users.noreply.github.com>
2025-08-18 09:02:36 -0300

    Update constants.js

--- app/javascript/dashboard/routes/dashboard/settings/macros/constants.js
+++ app/javascript/dashboard/routes/dashboard/settings/macros/constants.js
@@ -1,72 +1,72 @@
 export const MACRO_ACTION_TYPES = [
   {
     key: 'assign_team',
-    label: 'Assign a team',
+    label: 'Atribuir ao time',
     inputType: 'search_select',
   },
   {
     key: 'assign_agent',
-    label: 'Assign an agent',
+    label: 'Atribuir ao agente',
     inputType: 'search_select',
   },
   {
     key: 'add_label',
-    label: 'Add a label',
+    label: 'Adicionar etiquetas',
     inputType: 'multi_select',
   },
   {
     key: 'remove_label',
-    label: 'Remove a label',
+    label: 'Remover etiqueta',
     inputType: 'multi_select',
   },
   {
     key: 'remove_assigned_team',
-    label: 'Remove Assigned Team',
+    label: 'Remover time atribuído',
     inputType: null,
   },
   {
     key: 'send_email_transcript',
-    label: 'Send an email transcript',
+    label: 'Enviar uma transcrição por e-mail',
     inputType: 'email',
   },
   {
     key: 'mute_conversation',
-    label: 'Mute conversation',
+    label: 'Silenciar conversa',
     inputType: null,
   },
   {
     key: 'snooze_conversation',
-    label: 'Snooze conversation',
+    label: 'Adiar conversa',
     inputType: null,
   },
   {
     key: 'resolve_conversation',
-    label: 'Resolve conversation',
+    label: 'Resolver conversa',
     inputType: null,
   },
   {
     key: 'send_webhook_event',
-    label: 'Send Webhook Event',
+    label: 'Enviar webhook',
     inputType: 'url',
   },
   {
     key: 'send_attachment',
-    label: 'Send Attachment',
+    label: 'Enviar anexo',
     inputType: 'attachment',
   },
   {
     key: 'send_message',
-    label: 'Send a message',
+    label: 'Enviar uma mensagem',
     inputType: 'textarea',
   },
   {
     key: 'add_private_note',
-    label: 'Add a private note',
+    label: 'Adicionar uma nora privada',
     inputType: 'textarea',
   },
   {
     key: 'change_priority',
-    label: 'Change Priority',
+    label: 'Alterar prioridade',
     inputType: 'search_select',
   },
 ];
