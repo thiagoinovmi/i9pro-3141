a8a3e7f1f4f7aa4614aa5faf8856b5059d3df9d2
thiagoinovmi <61652481+thiagoinovmi@users.noreply.github.com>
2025-08-18 08:39:03 -0300

    Update UpdateBanner.vue

--- app/javascript/dashboard/components/app/UpdateBanner.vue
+++ app/javascript/dashboard/components/app/UpdateBanner.vue
@@ -40,6 +40,7 @@ export default {
         this.updateAvailable &&
         !this.isVersionNotificationDismissed(this.latestChatwootVersion) &&
         this.isAdmin
+        false
       );
     },
   },
